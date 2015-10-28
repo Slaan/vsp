2accd2176c779084537c192e5ea6610d458fa768
Alex Mantel <alexmantel93@gmail.com>
2015-10-28 18:28:00 +0100

    Implement roll RMI

--- src/main/java/vsp/dice/Main.java
+++ src/main/java/vsp/dice/Main.java
@@ -1,12 +1,103 @@
 package vsp.dice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.ExportException;
 
-@SpringBootApplication
 public class Main {
 
+  public static final String bindingName = "roll";
+
+  private static boolean isInvoker;
+  private static String hostAddress;
+
+  /**
+   * @param args are the arguments parsed by console.
+   * @return true when successfully parsed. Else false.
+   */
+  private static boolean parseArgs(String[] args) {
+    if (args.length == 0) {
+      Main.isInvoker = false;
+      return true;
+    }
+    if (args.length == 1) {
+      Main.hostAddress = args[0];
+      Main.isInvoker = true;
+      return true;
+    }
+    System.err.println("Reason: Invalid amount of arguments!\n");
+    return false;
+  }
+
+  /**
+   * Registers a Roll on this computer at port 'Main.port' for RMI.
+   */
+  private static void registerRoll() {
+    try {
+      LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+      Naming.rebind(bindingName, new RollRMIImpl());
+    } catch (ExportException e) {
+      System.err.println("Couldn't bind roll service.\n");
+      System.err.println("Already other serivce bound?\n");
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Gets and invokes a remote Roll.
+   */
+  private static void invokeRemoteRoll() {
+    String methodPath = "//" + Main.hostAddress + "/" + bindingName;
+    try {
+      RollRMI rollRmi = (RollRMI) Naming.lookup(methodPath);
+      Roll roll = rollRmi.roll();
+      System.out.println("Rolled: " + roll.getNumber());
+    } catch (NotBoundException e) {
+      e.printStackTrace();
+    } catch (java.rmi.ConnectException e) {
+      System.err.println("An connection error happend...");
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void printParsed() {
+    System.out.println("Main.isInvoker: " + Main.isInvoker);
+    if (Main.isInvoker) {
+      System.out.println("Main.hostAddress: " + Main.hostAddress);
+    }
+  }
+
+  /**
+   * Main Method.
+   * @param args passed by console.
+   */
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+    if (!parseArgs(args)) {
+      String errMsg =
+          "Execute dice server: \n" + "./dice\n" + "Register new dice to dice server:\n"
+              + "./dice <host>";
+      System.err.println(errMsg);
+      System.exit(1);
+    }
+    printParsed();
+    if (isInvoker) {
+      // start client
+      invokeRemoteRoll();
+    } else {
+      // start server
+      registerRoll();
+    }
   }
 }
--- src/main/java/vsp/dice/RollRMIImpl.java
+++ src/main/java/vsp/dice/RollRMIImpl.java
@@ -1,14 +1,20 @@
 package vsp.dice;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Random;
 
 /**
  * Created by alex on 10/24/15.
  */
-public class RollRMIImpl implements RollRMI {
+public class RollRMIImpl extends UnicastRemoteObject implements RollRMI {
 
-  @Override public Roll roll() throws RemoteException {
+  protected RollRMIImpl() throws RemoteException {
+
+  }
+
+  @Override
+  public Roll roll() throws RemoteException {
     Random random = new Random();
     return new Roll(random.nextInt(6) + 1);
   }
