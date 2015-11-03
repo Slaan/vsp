f0e07ba640e9689960b9d855512b72022aaf7aec
Alex Mantel <alexmantel93@gmail.com>
2015-11-03 23:45:38 +0100

    Rename main class and fix wrong checkstyle path

---
+++ dice/src/main/java/vsp/dice/MyMainClass.java
@@ -0,0 +1,12 @@
+package vsp.dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMainClass {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyMainClass.class, args);
+  }
+}
